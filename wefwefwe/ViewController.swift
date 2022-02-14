6d118534e8ebda4cb66900ac6b41af50a5074e09
Gagick <gagick.arutyunyan0@gmail.com>
2022-02-14 15:45:08 +0400

    Initial Commit

---
+++ wefwefwe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wefwefwe
+//
+//  Created by Gagik Harutyunyan on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var x = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        x+2
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
